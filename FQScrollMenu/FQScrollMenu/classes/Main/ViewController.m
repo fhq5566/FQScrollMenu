ab25dbfd0da6109458f1f966906989fd696a9824
lx <lx@lxdeMacBook-Pro.local>
2017-07-26 10:39:55 +0800

    添加FQScrollMenu.podspec到git

--- FQScrollMenu/FQScrollMenu/classes/Main/ViewController.m
+++ FQScrollMenu/FQScrollMenu/classes/Main/ViewController.m
@@ -29,33 +29,26 @@
         NSInteger index = (i + 1);
         vc1.content = [NSString stringWithFormat:@"我是VC %ld" , index];
         
-        NSString *buttonText = @"";
-        if (i == 2) {
-            buttonText = [NSString stringWithFormat:@"操作操作%ld", index];
-        }else if(i == 5){
-            buttonText = [NSString stringWithFormat:@"操作作%ld", index];
-        }else if(i == 8){
-            buttonText = [NSString stringWithFormat:@"操作操作操作%ld", index];
-        }else{
-            buttonText = [NSString stringWithFormat:@"操作%ld", index];
-        }
-        //@"order_arrow2_btn"
+        NSString *buttonText = [NSString stringWithFormat:@"操作%ld", index];
         FQBtmLineButton *titleButton1 = [[FQBtmLineButton alloc] initWithTitle:buttonText icon:@"order_arrow2_btn" showVC:vc1];
+        //正常颜色和选中颜色
         [titleButton1 setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
         [titleButton1 setTitleColor:[UIColor redColor] forState:UIControlStateSelected];
+        //如果需要更改高度和颜色，
         titleButton1.lineH = 2;
         titleButton1.lineBackgroundColor = [UIColor redColor];
         titleButton1.titleLabel.font = [UIFont systemFontOfSize:14];
         [buttons addObject:titleButton1];
     }
+    //还有数组修改方法哦- (void)setupLineH:(CGFloat)lineH lineBackgroundColor:(UIColor *)lineBackgroundColor;
+//    [buttons setupLineH:2 lineBackgroundColor:[UIColor redColor]];
     
     FQScrollMenu *scrollMenu = [[FQScrollMenu alloc] initWithStyle:FQScrollMenuHeaderStyleScroll selectedIndex:3 bottomHeight:0 superVC:self];
     scrollMenu.delegate = self;
     [self.view addSubview:scrollMenu];
     scrollMenu.menuButtons = buttons;
-    scrollMenu.contentScrollView.scrollEnabled = YES;
-    
-//    scrollMenu.selectedIndex = 5;
+    scrollMenu.contentScrollView.scrollEnabled = YES;  //内容是否可滚动
+//    scrollMenu.selectedIndex = 5;  //选中下标
     
     [scrollMenu mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.mas_equalTo(50);
